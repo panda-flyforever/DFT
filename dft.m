a5efbe581f6ec5c4c3a76dd250779e6d70d73ce2
sharmarahul17 <44519716+sharmarahul17@users.noreply.github.com>
2018-10-29 13:44:23 -0600

    Add files via upload

--- dft.m
+++ dft.m
@@ -1,28 +1,38 @@
+% program: dtf.m
+% author: Rahul Sharma
+% course: CS 827
+% date: 2018/10/29
+% assignment #2
+% description: this program shows how to calculate DFT of a signal using twiddle  
+% factor W and the matrix calculation in MATLAB. Input of this program is amplitudes.dat  
+% file which contains all the amplitudes. Output is plotting of graphs along with the  
+% frequencies calculates are expoirted to an output.txt file.
+%---------------------------------------------------------------------------------------
+
 x = dlmread('amplitudes.dat'); % Read list of amplitudes from a data file 
 N = length(x);
 X = zeros(N,1); % Initializing X with a matrix of N-by-1
-
-t=0:N-1;
-k=t;
+t=0:N-1;k=t;
 tic
-W=exp(-j*2*pi/N*(t'*k)); % Twiddle factors matrix
+% Twiddle factor W and matrix calculation
+W=exp(-j*2*pi/N*(t'*k)); 
 X=W*x'; % Compute the DFT as a product of the twiddle factors matrix with input vector x
 timeElapsed = toc
 
-subplot(311); % divides the current figure into a 3-by-1 grid and creates axes for position 1
+% divides the current figure into a 3-by-1 grid and creates axes for position 1
+subplot(311); % plots entries in Time against corresponding entries of Amplitude
 stem(t,x); % Plot discrete sequence data
 xlabel('Time (s)'); % show label on x axis
 ylabel('Amplitude'); % show label on y axis
 title('Time domain - Input sequence') % show title of graph
 
-
-subplot(312); 
+subplot(312); % plots magnitude in frequency domain
 stem(t,X); 
 xlabel('Frequency');
 ylabel('|X(k)|');
 title('Frequency domain - Magnitude response')
 
-subplot(313); 
+subplot(313); % plots phase in frequency domain
 stem(t,angle(X)); 
 xlabel('Frequency');
 ylabel('Phase');
@@ -30,4 +40,5 @@ title('Frequency domain - Phase response');
 
 X         % to check |X(k)|
 angle(X)  % to check phase
-csvwrite('output.txt',X);
+% saving the DFT output frequencies in a text file names output.txt
+csvwrite('output.txt',X); 
